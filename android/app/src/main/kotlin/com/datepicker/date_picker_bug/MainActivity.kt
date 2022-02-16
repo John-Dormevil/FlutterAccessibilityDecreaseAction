3905d4dd8a6647a9391318b2e9bc331bc036d28a
Jean-Jules DORMEVIL <jdormevil@oui.sncf>
2022-02-16 11:19:36 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/datepicker/date_picker_bug/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.datepicker.date_picker_bug
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Semantic Action Decrease Bug'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late DateTime? date;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Expanded(
+              child: CupertinoDatePicker(
+                  minimumDate: DateTime.now().subtract(const Duration(days: 10)),
+                  initialDateTime: DateTime.now().add(const Duration(days: 2)),
+                  maximumDate: DateTime.now().add(const Duration(days: 10)),
+                  onDateTimeChanged: (DateTime newDate) {
+                    setState(() => date = newDate);
+                  }),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
